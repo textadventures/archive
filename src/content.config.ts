a0536c1f791b1c29d910d8923c9a7bff088866f6
Alex Warren <alex@alexwarren.uk>
2025-01-28 23:27:58 +0000

    Adding forum details

--- src/content.config.ts
+++ src/content.config.ts
@@ -33,7 +33,42 @@ const getForumCollection = (forumId: string) => defineCollection({
     }),
 });
 
-export const allForums = [ "design", "games", "general", "quest", "questkit", "samples", "site", "squiffy" ];
+export const forumDetails: Record<string, { title: string, replacement: string }> = {
+    "quest": {
+        title: "Quest",
+        replacement: "For Quest, please create new topics over at <a href=\"https://github.com/textadventures/quest/discussions\">GitHub Discussions for the Quest repo</a>."
+    },
+    "samples": {
+        title: "Libraries and Code Samples",
+        replacement: "For Libraries and Code Samples, please create new topics over at <a href=\"https://github.com/textadventures/quest/discussions/categories/libraries-and-code-samples\">GitHub Discussions for the Quest repo</a> (Libraries and Code Samples category)."
+    },
+    "questkit": {
+        title: "Quest 6 and QuestJS",
+        replacement: "For Quest 6 and QuestJS, please create new topics over at <a href=\"https://github.com/ThePix/QuestJS/discussions\">GitHub Discussions for the QuestJS repo</a>."
+    },
+    "squiffy": {
+        title: "Squiffy",
+        replacement: "For Squiffy, please create new topics over at <a href=\"https://github.com/textadventures/squiffy/discussions\">GitHub Discussions for the Squiffy repo</a>."
+    },
+    "games": {
+        title: "Game Announcements and Feedback",
+        replacement: "For Game Announcements and Feedback, post in GitHub Discussions for <a href=\"https://github.com/textadventures/quest/discussions\">Quest</a> or <a href=\"https://github.com/textadventures/squiffy/discussions\">Squiffy</a>, or over at <a href=\"https://intfiction.org/c/playing/project-announcements/50\">intfiction.org</a>."
+    },
+    "design": {
+        title: "Interactive Fiction and Game Design",
+        replacement: "For Interactive Fiction and Game Design, we suggest posting at <a href=\"https://intfiction.org/c/authoring/general-design-discussions/17\">intfiction.org</a>."
+    },
+    "general": {
+        title: "General Discussion",
+        replacement: "For general discussion, please create new topics over at <a href=\"https://github.com/orgs/textadventures/discussions\">GitHub Discussions</a>."
+    },
+    "site": {
+        title: "Site Announcements and Feedback",
+        replacement: "For discussion about this website, please create new topics over at <a href=\"https://github.com/orgs/textadventures/discussions\">GitHub Discussions</a>."
+    }
+};
+
+export const allForums = Object.keys(forumDetails);
 const design = getForumCollection("design");
 const games = getForumCollection("games");
 const general = getForumCollection("general");
