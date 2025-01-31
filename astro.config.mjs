c558948b4848e641f286ad936f9f6899e3151dca
Alex Warren <alex@alexwarren.uk>
2025-01-31 17:27:21 +0000

    Add pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import pagefind from "astro-pagefind";
+
+export default defineConfig({
+  build: {
+    format: "file",
+  },
+  integrations: [pagefind()],
+});
